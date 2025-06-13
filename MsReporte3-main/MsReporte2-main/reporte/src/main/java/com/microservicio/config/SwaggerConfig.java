b3f51c94276c072470e9ac0870a5fe2af7432f95
Benjatapia20 <be.tapiar@duocuc.cl>
2025-06-13 16:05:56 -0400

    Update SwaggerConfig.java

--- MsReporte3-main/MsReporte2-main/reporte/src/main/java/com/microservicio/config/SwaggerConfig.java
+++ MsReporte3-main/MsReporte2-main/reporte/src/main/java/com/microservicio/config/SwaggerConfig.java
@@ -1,5 +1,19 @@
 package com.microservicio.config;
 
-public class SwaggerConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+  @Bean
+  public OpenAPI openAPI(){
+    return new openAPI().info(new Info()
+      .title(title:"Proyecto De Usuarios")
+      .version(version:"1.0")
+      .descripcion(description:"Este es el micro servicio de Reportes")
+      );
+}
 }
