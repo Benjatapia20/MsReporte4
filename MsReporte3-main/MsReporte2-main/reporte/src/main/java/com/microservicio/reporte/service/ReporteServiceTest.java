87bca379f3fbaea501d935cc47aa195d5f7c9e96
Benjatapia20 <be.tapiar@duocuc.cl>
2025-06-13 18:45:46 -0400

    Update ReporteServiceTest.java

--- MsReporte3-main/MsReporte2-main/reporte/src/main/java/com/microservicio/reporte/service/ReporteServiceTest.java
+++ MsReporte3-main/MsReporte2-main/reporte/src/main/java/com/microservicio/reporte/service/ReporteServiceTest.java
@@ -1,5 +1,36 @@
 package com.microservicio.reporte.service;
 
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.microservicio.reporte.model.Reporte;
+import com.microservicio.reporte.model.entity.ReporteEntity;
+import com.microservicio.reporte.repository.ReporteRepository;
+
+@SpringBootTest
 public class ReporteServiceTest{
+
+  @Mock
+  private ReporteRepository reporteRepository;
+
+  @InjectMocks
+  private ReporteService reporteService;
   
+    @Test
+    public void testCrearReporte_Success() {
+        Reporte reporte = new Reporte(0, "Reporte1", true, LocalDate.now(), "TipoA", "Usuario1");
+
+        when(reporteRepository.existsByDescripcion("Reporte1")).thenReturn(false);
+        when(reporteRepository.save(any(ReporteEntity.class))).thenReturn(new ReporteEntity());
+
+        String resultado = reporteService.crearReporte(reporte);
+
+        assertEquals("Reporte Creado Correctamente", resultado);
+        verify(reporteRepository, times(1)).save(any(ReporteEntity.class));
+    }
 }
